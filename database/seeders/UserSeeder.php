fb98e3583e2665de6d198b4d54b4b39b3eeaea49
yamauchi-bd <167970876+yamauchi-bd@users.noreply.github.com>
2024-08-11 13:43:46 +0900

    ログイン・ユーザー登録ページまで

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'email' => 'dio@example.com',
+            'password' => Hash::make('password'),
+          ]);
+          User::create([
+            'email' => 'kira@example.com',
+            'password' => Hash::make('password'),
+          ]);
+          User::create([
+            'email' => 'gyro@example.com',
+            'password' => Hash::make('password'),
+          ]);
+      
+    }
+}
