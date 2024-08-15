fff17e74d6976e76c940377605d8ea3c8970e011
kakaori <kaoneko6622@gmail.com>
2024-08-15 12:28:33 +0900

    feature_top_#30

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="w-full">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto">
+        <ul
+          class="text-sm flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-10 mb-2 border-b border-gray-200">
+          <li><a href="#" class="text-gray-500 hover:text-gray-900">運営会社</a></li>
+          <li><a href="#" class=" text-gray-500 hover:text-gray-900">利用規約</a></li>
+          <li><a href="#" class=" text-gray-500 hover:text-gray-900">プライバシーポリシー</a></li>
+          <li><a href="#" class=" text-gray-500 hover:text-gray-900">利用ポリシー</a></li>
+          <li><a href="#" class=" text-gray-500 hover:text-gray-900">投稿ガイドライン</a></li>
+          <li><a href="#" class=" text-gray-500 hover:text-gray-900">報告•お問合せ</a></li>
+        </ul>
+
+        <span class="text-sm text-gray-400 text-center block my-4">©<a href="https://pagedone.io/">vivivision</a> 2024,
+          All
+          rights reserved.</span>
+      </div>
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/pagedone@1.1.2/src/js/pagedone.js"></script>
+  @vite(['resources/js/top.js'])
+</body>
+
+</html>
